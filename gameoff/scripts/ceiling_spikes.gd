797c923a4e2582770bde32d4b1b4f7c6a861c0fd
AlecF206 <alec.joseph.freeman@gmail.com>
2024-11-07 17:39:18 -0500

    added some traps

---
+++ gameoff/scripts/ceiling_spikes.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+@export var damage := 25
+@export var wait_time := 2
+
+func _on_body_entered(body: Node2D) -> void:
+	body.take_damage(damage)
+
+func _ready() -> void:
+	crush()
+
+func crush():
+	$AnimationPlayer.play("smash")
+	await get_tree().create_timer(wait_time, false).timeout
+	crush()
