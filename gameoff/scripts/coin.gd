6527817e267e23e7e2698494445a9b6900f98c00
AlecF206 <alec.joseph.freeman@gmail.com>
2024-11-07 21:18:32 -0500

    did some level design and added healing to coins

--- gameoff/scripts/coin.gd
+++ gameoff/scripts/coin.gd
@@ -1,6 +1,8 @@
 extends Area2D
 
-func _on_body_entered(body: Node2D) -> void:
+@onready var ui := get_parent().get_parent().find_child("Player").find_child("CanvasLayer").get_child(0)
+
+func _on_body_entered(_body: Node2D) -> void:
 	Global.secrets_found += 1
+	ui.time -= 10
 	queue_free()
-	## - time or healing ##
